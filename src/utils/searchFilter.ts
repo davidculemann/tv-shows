fcabdad844bba3eb70ecbcd8dc7901ef2f51921d
David Culemann <davidculemann@gmail.com>
2021-11-03 13:02:03 +0400

    Fix to filter util

--- src/utils/searchFilter.ts
+++ src/utils/searchFilter.ts
@@ -1,23 +1,10 @@
-interface IEpisode {
-  // id: number;
-  // url: string;
+interface FilterEpisode {
   name: string;
-  season: number;
-  number: number;
-  // type: string;
-  // airdate: string;
-  // airtime: string;
-  // airstamp: string;
-  // runtime: number;
-  image: {
-    medium: string;
-    original: string;
-  };
   summary: string;
-  // _links: { self: { href: string } };
 }
 
-export const searchFilter = (episode: IEpisode, search: string): boolean =>
-  episode.name.includes(search) ||
-  episode.summary.includes(search) ||
-  search === "";
+export const searchFilter =
+  (episode: FilterEpisode, search: string): boolean =>
+    episode.name.includes(search) ||
+    episode.summary.includes(search) ||
+    search === ""
