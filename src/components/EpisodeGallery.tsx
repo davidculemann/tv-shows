d51a04646b616bfba01d134a617d1e06c6773738
David Culemann <davidculemann@gmail.com>
2021-11-03 18:02:17 +0400

    Fixed font and typo

--- src/components/EpisodeGallery.tsx
+++ src/components/EpisodeGallery.tsx
@@ -20,9 +20,9 @@ export function EpisodeGallery(): JSX.Element {
             setSearch(event.target.value);
           }}
         />
-        &nbsp; showing{" "}
-        {episodes.filter((epi) => searchFilter(epi, search)).length} out of{" "}
-        {episodes.length} episodes
+        &nbsp; <span style={{ fontSize: "13px" }}>showing{" "}
+          {episodes.filter((epi) => searchFilter(epi, search)).length} out of{" "}
+          {episodes.length} episodes</span>
       </div>
       <div
         style={{
@@ -49,34 +49,3 @@ export function EpisodeGallery(): JSX.Element {
     </>
   );
 }
-
-/*
-<Episode
-        name={"Winter is coming"}
-        season={1}
-        number={1}
-        image={{
-          medium:
-            "https://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg",
-          original:
-            "https://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg",
-        }}
-        summary={
-          "Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage"
-        }
-      />
-      <Episode
-        name={"The Kingsroad"}
-        season={1}
-        number={2}
-        image={{
-          medium:
-            "https://static.tvmaze.com/uploads/images/medium_landscape/1/2669.jpg",
-          original:
-            "https://static.tvmaze.com/uploads/images/medium_landscape/1/2669.jpg",
-        }}
-        summary={
-          "An incident on the Kingsroad threatens Eddard and Robert's friendship. Jon and Tyrion travel to the Wall, where they discover that the reality of the Night's Watch may not match the heroic image of it."
-        }
-      />
-      */
--- src/components/EpisodeStyle.tsx
+++ src/components/EpisodeStyle.tsx
@@ -4,8 +4,8 @@ export const episodeStyle: React.CSSProperties = {
   width: "90%",
   height: "auto",
   padding: "10px",
-  marginRight: "auto",
-  marginLeft: "auto",
+  marginRight: "10px",
+  marginLeft: "10px",
   marginTop: "10px",
   marginBottom: "10px",
 };
